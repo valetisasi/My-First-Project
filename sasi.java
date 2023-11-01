7311880c3a218256a14b19cebb9d42d06e174cbd
valetisasi <valetisasi@gmail.com>
2023-11-01 09:35:36 +0530

    Added My First Project

--- sasi.java
+++ sasi.java
@@ -1,7 +1,8 @@
 public class sasi {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("Hello world");
+
+        System.out.println("Sasi kumar");
     }
 
 }
