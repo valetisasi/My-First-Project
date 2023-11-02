145ce8fb261447814a99e6fbc7381ce369999eb0
valetisasi <valetisasi@gmail.com>
2023-11-02 09:32:24 +0530

    Added my Second project

---
+++ Example.java
@@ -0,0 +1,59 @@
+public class Example {
+    public class Exercise {
+        private String name;
+        private int difficultyLevel;
+        private int durationMinutes;
+        private String description;
+
+        public Exercise(String name, int difficultyLevel, int durationMinutes, String description) {
+            this.name = name;
+            this.difficultyLevel = difficultyLevel;
+            this.durationMinutes = durationMinutes;
+            this.description = description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getDifficultyLevel() {
+            return difficultyLevel;
+        }
+
+        public int getDurationMinutes() {
+            return durationMinutes;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setDifficultyLevel(int difficultyLevel) {
+            // You can add validation here to ensure the difficulty level is within the range (1-5)
+            this.difficultyLevel = difficultyLevel;
+        }
+
+        public void setDurationMinutes(int durationMinutes) {
+            // You can add validation here to ensure the duration is non-negative
+            this.durationMinutes = durationMinutes;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return "Exercise{" +
+                    "name='" + name + '\'' +
+                    ", difficultyLevel=" + difficultyLevel +
+                    ", durationMinutes=" + durationMinutes +
+                    ", description='" + description + '\'' +
+                    '}';
+        }
+    }
+}
